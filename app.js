b850071605f7682497b2768309f63a1df1d48a9a
idrees khan <midreeskhan7276@gmail.com>
2023-09-08 22:59:32 +0500

    done

---
+++ app.js
@@ -0,0 +1,415 @@
+// alert("hello world")
+
+
+                      //?      chapter = 3    ////////
+
+
+
+                        //*   Question  =  1   //////
+
+
+
+
+// var para = 18;
+// alert("I am " + para + " years old")
+
+
+
+
+
+                        //*   Question  = 2   /////
+
+ 
+
+
+
+// var a = "You have visited this site";
+// alert(a +"1 times")
+// alert(a +"2 times")
+// alert(a +"3 times")
+// alert(a + "4 times")
+// alert(a + "5 times")
+// alert(a + "6 times")
+// alert(a + "7 times")
+// alert(a + "8 times")
+// alert(a + "9 times")
+// alert(a + "10 times")
+// alert(a + "11 times")
+// alert(a + "12 times")
+// alert(a + "13 times")
+// alert(a + "14 times")
+
+
+
+
+
+
+                ///*   Question  = 3   //////
+
+
+
+
+// var hbd =2002;
+// document.write("My brithday is " + hbd);
+
+
+
+
+
+              ///*   Question  = 4   //////
+ 
+
+
+
+  // var name= "John Doe";
+// var num= 5;
+// var text ="T-shirt"
+// document.write(name + " ordered " + num + text+ " (s) on XYZ Clothing store")
+
+
+
+
+
+
+                    //?      chapter = 4              //////////
+
+
+
+
+
+                  ///*   Question  = 1   //////
+
+
+
+
+// var tax ="#" ,
+// var tax1 ="#" ,
+// var tex2 = "#" ;
+
+
+
+
+
+                   ///*   Question  = 2   //////
+
+
+
+
+
+//  legal variable names
+
+//  var firstName;
+//  var first_name;
+//  var $name;
+//  var first100name;
+//  var firstName100;
+// illegal variable names 
+// var ?name;
+// var first-name;
+// var 10name;
+// var first name;
+// var &firstname;
+
+
+
+
+
+
+
+                    //*   Question  = 3   //////
+
+
+
+
+
+// document.write("Rules for naming JS variables <br> <br>")
+// document.write("Variable names can only contain, numbers, $ and _ .For example : $my_ 1stVariable <br>")
+// document.write("Variables must begin with a letter , $ or _. For example $name, _name or name <br>")
+// document.write("Variable names are case sensitive <br>")
+// document.write("Variable names should not be JS keywords")
+
+
+
+
+
+
+                    //?             chapter = 5      //////////////
+
+
+
+
+
+
+                      //*   Question  = 1  //////
+
+
+
+        // var num=3;
+// var num1=5;
+// var  add= num+num1;
+// document.write("Sum of " + num +" and " + num1 + " is " + add)
+
+
+
+
+
+
+                           //*   Question  = 2   //////
+
+
+
+
+
+// Subtract questions
+
+// var num=3;
+// var num1=5;
+// var  sub= num-num1;
+// document.write("Subtract of " + num +" and " + num1 + " is " + sub)
+
+// Multiplication questions
+// var num=3;
+// var num1=5;
+// var  mul= num*num1;
+// document.write("Multiplication of " + num +" and " + num1 + " is " + mul)
+
+// Percentage question
+// var num=3;
+// var num1=5;
+// var  per= num/num1;
+// document.write("Percentage of " + num +" and " + num1 + " is " + per)
+
+// Modulus question
+
+// var num=4;
+// var num1=5;
+// var mod= num % num1;
+// document.write("Modulus of " + num +" and " + num1 + " is " + mod)
+
+    
+
+
+
+                    //*   Question  = 3   //////
+ 
+
+
+
+
+
+// var num;
+//  num=5;
+//  document.write("Initial value : " + num + "<br>")
+//  var incre = ++num;
+
+//  document.write("value after increment is : " + incre +"<br>" )
+//  var add = 7;
+//  var add1 = incre + add;
+//  document.write("Value after addition is : " + add1 +"<br>")
+//  var decre = --add1;
+//   document.write("Value after decrement is : " + decre + "<br>")
+//   var remin = decre%3;
+//   document.write("Rminder is : " + remin)
+
+
+
+
+
+
+
+
+                         //*  Question  = 4   //////
+
+
+
+
+
+      // var price = 600;
+// var total = 5;
+// var multi = price*total;
+// document.write("Total cost to buy 5 ticket to a movie is : " + multi +"PKR")  
+
+
+
+
+
+
+
+                      //*     question = 5    ///////
+
+
+
+
+
+ 
+// var a = 2 * 1;
+// var b = 2 * 2;
+// var c = 2 * 3;
+// var d = 2 * 4;
+// var e = 2 * 5;
+// var f = 2 * 6;
+// var g = 2 * 7;
+// var h = 2 * 8;
+// var i = 2 * 9;
+// var j = 2 * 10;
+// document.write("2 x 1 = " + a + "<br>"+"2 x 2 = " + b + "<br>" 
+// +"2 x 3 = " + c + "<br>" +"2 x 4 = " + d + "<br>" +"2 x 5 = " + e + 
+// "<br>" +"2 x 6 = " + f + "<br>" +"2 x 7 = " + g + "<br>" +"2 x 8 = " + h + 
+// "<br>" +"2 x 9 = " + i + "<br>" +"2 x 10 = " + j + "<br>")
+
+
+
+
+
+                    //*    quetsion = 6    ////////
+
+
+
+
+
+
+
+// var cel = 70;
+// var a = "F";
+// var b = 9;
+// var c = 5;
+// var d = 32;
+// var calc =   (cel * b/c) + d ;
+// document.write(cel+ "C "+ " is " +calc +  a + "<br>")
+
+// var fren = 75;
+// var e = "C";
+// var f = 32;
+// var g = 5;
+// var h = 9;
+// var calcu =   (fren - f ) * g/h;
+// document.write(fren + "F "+ "  is  " +calcu +  e)
+
+
+
+
+
+
+
+                       //*   question = 7    //////
+
+
+
+
+
+
+
+
+// var a = 650;
+// var b = 3;
+// var c = 100;
+// var d = 7;
+// var e = 100;
+// var total = a * b + c * d + e ;
+// document.write("Price of item 1 is" + a + "<br>" + "Quatity of item 1 is " + b + "<br>"
+// +"Price of item 2 is" + c + "<br>" + "Quatity of item 2 is " + d + "<br>" + "<br>" + "<br>" + "<br>" )
+// document.write( "Total cost of your order is "   +total)
+
+
+
+
+
+
+
+                       //* question = 8    /////////
+
+
+ 
+
+// var obtain = 500;
+// var total  = 800;
+// var perc = obtain / total *100
+// document.write("Total Marks : " + total  +  "<br>"+"Marks Obtained : " 
+// + obtain + "<br>" + "Percentage : " + perc +"%")
+
+
+
+
+
+                         //*  question = 9    //////////
+
+
+
+
+// var currency = (10*104.80)+(25*28)
+// document.write("Total currency in PKR : " + currency)
+
+
+
+
+
+                  // *    question = 10      /////////
+
+
+
+
+                  
+
+// var calc = 5 *10 /2
+// document.write(calc)
+
+
+
+
+
+                     // *  question = 11     ////////
+   
+
+
+
+
+// var year = 2000 ;
+// var current = 2021;
+// var age = current-year;
+// document.write("Crrent Year : " + current + "<br>"+
+// "Birthyear : " + year + "<br>" + " Your age is : " + age)
+
+
+
+
+
+
+
+                       //*    question = 12    //////// 
+
+
+
+
+
+
+
+//   for circumference...radius.
+
+//     var pi = 3.142 ;
+//     var radius = 20;
+//     var circumf = 2 * pi * radius;
+// document.write("Radius of a circle is : " +radius + "<br>"+ "<br>")
+// document.write("The sircumference is : " + circumf + "<br>"+ "<br>")
+     // for Area... 
+
+//     var radius1 = 400
+//     var area = pi * radius1
+//     document.write("The Area is : " + area )
+
+   
+
+
+
+
+                    //*   question - 13              //////// 
+
+
+
+//     var fav = "lays";
+//     var curAge = 21;
+//     var estMaxAge = 150;  //1 year =365 day (365*150) =54750 days  //allah ko pata h 
+//      var perDay = 3;
+//      var calc = (estMaxAge*365) * perDay;
+// document.write("Favorite snacks :" + fav + "<br>" + "Current Age : " + curAge + "<br>"
+//  + "Estinated Maximum Age : " + estMaxAge + "<br>" + " Amount of snack per day : " + perDay + "<br>"
+//  + " You will need " + calc + " " + fav + " to last you until the ripe old age of " + estMaxAge )
